e93c51cd7b99add0cbf5d751dd575266873068ab
Talent-Roy <53985232+Talent-Roy@users.noreply.github.com>
2021-07-06 00:54:10 +0100

    initial commit

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -30,7 +30,7 @@ export const login = async (email, password) => {
   try {
     const res = await axios({
       method: 'POST',
-      url: 'http://localhost:8080/api/v1/users/login',
+      url: 'http://handymanng/api/v1/users/login',
       data: {
         email,
         password
@@ -52,7 +52,7 @@ export const logout = async () => {
   try {
     const res = await axios({
       method: 'GET',
-      url: 'http://localhost:8080/api/v1/users/logout'
+      url: 'http://handymanng/api/v1/users/logout'
     });
     if ((res.data.status = 'success')) location.replace('/');
   } catch (err) {
