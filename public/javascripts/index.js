dd4d25838ad519a2ba12a1fca76f114b1778aac5
Talent-Roy <53985232+Talent-Roy@users.noreply.github.com>
2021-07-19 02:31:27 +0100

    initial commit

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -11,7 +11,7 @@ const signupForm = document.querySelector('.signup');
 if (signupForm)
   signupForm.addEventListener('submit', e => {
     e.preventDefault();
-    const name = document.getElementById('email').value;
+    const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
     const confirmPassword = document.getElementById('confirmPassword').value;
